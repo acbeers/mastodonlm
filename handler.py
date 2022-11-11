a7717a76878753f73d6012d05cb3045fa64c6eb0
Andrew Beers <acbeers@gmail.com>
2022-11-11 12:43:19 -0800

    Add oAuth flow to allow any user at hachyderm to use this.

--- handler.py
+++ handler.py
@@ -1,6 +1,13 @@
 """Handler for list manager functions"""
 
+import datetime
 import json
+import time
+import uuid
+
+import boto3
+
+
 from mastodon import (
     Mastodon,
     MastodonAPIError,
@@ -10,18 +17,56 @@ from mastodon import (
 from cfg import Config
 
 
-def get_mastodon():
+# We hold mappings of users to tokens in a database.
+# For testing, this is our stupid database.
+
+
+class Database:
+    """Simple database interface that abstracts Dynamo"""
+
+    _data = {}
+    _client = boto3.client("dynamodb")
+
+    @staticmethod
+    def get(cookie):
+        """Gets the auth token associated with the cookie"""
+        resp = Database._client.get_item(
+            TableName="authTable", Key={"key": {"S": cookie}}, AttributesToGet=["token"]
+        )
+        if "Item" in resp:
+            return resp["Item"]["token"]["S"]
+        return None
+
+    @staticmethod
+    def set(cookie, token):
+        """Sets the auth token associated with the cookie"""
+        now = datetime.datetime.now()
+        expire = now + datetime.timedelta(days=1)
+        unix = time.mktime(expire.timetuple())
+        item = {
+            "key": {"S": cookie},
+            "token": {"S": token},
+            "expires_at": {"N": str(unix)},
+        }
+        Database._client.put_item(TableName="authTable", Item=item)
+
+
+def parse_cookies(cookies):
+    """Do a simple parse of a list of cookies, turning it into a name:value dict"""
+    res = {}
+    for cookie in cookies:
+        arr = cookie.split(";")
+        (name, val) = arr[0].split("=")
+        res[name] = val
+    return res
+
+def get_mastodon(cookie):
+    token = Database.get(cookie)
     mastodon = Mastodon(
         client_id=Config.client_id,
         client_secret=Config.client_secret,
-        access_token=Config.access_token,
+        access_token=token,
         api_base_url="https://hachyderm.io",
-    )
-    mastodon.log_in(
-        username=Config.username,
-        password=Config.password,
-        scopes=["read:lists", "read:follows", "read:accounts", "write:lists"],
-        to_file="pytooter_usercred.secret",
     )
     return mastodon
 
@@ -43,9 +88,15 @@ def get_all(func, *args):
 
 
 def info(event, context):
-    """Returns info for a"""
+    """
+    Handler to get the lists and follower information that the webapp needs.
+    """
+
+    cookies = parse_cookies(event["cookies"])
+    cookie = cookies["list-manager-cookie"]
+
     try:
-        mastodon = get_mastodon()
+        mastodon = get_mastodon(cookie)
     except MastodonIllegalArgumentError:
         return {"statusCode": 500, "body": "ERROR"}
     except MastodonInternalServerError:
@@ -97,9 +148,76 @@ def info(event, context):
     return json.dumps(output)
 
 
+def auth(event, context):
+    """
+    Handler for the start of an authentication flow.
+    """
+    # First, see if we have an active session
+    cookies = parse_cookies(event["cookies"])
+    cookie = cookies.get("list-manager-cookie", None)
+    if cookie is not None:
+        try:
+            test = get_mastodon(cookie)
+            test.me()
+            print("Already logged in")
+            return {"statusCode": 200, "body": json.dumps({"status": "OK"})}
+        except MastodonAPIError:
+            # If here, we aren't logged in, so drop through to start the
+            # oAuth flow.
+            pass
+
+    # TODO: Map this to a place where I store secrets.
+    mastodon = Mastodon(
+        client_id=Config.client_id,
+        client_secret=Config.client_secret,
+        access_token=Config.access_token,
+        api_base_url="https://hachyderm.io",
+    )
+    url = mastodon.auth_request_url(
+        scopes=["read:lists", "read:follows", "read:accounts", "write:lists"],
+        redirect_uris="http://localhost:3000/callback",
+    )
+    return {"statusCode": 200, "body": json.dumps({"url": url})}
+
+
+def callback(event, context):
+    code = event["queryStringParameters"]["code"]
+    mastodon = Mastodon(
+        client_id=Config.client_id,
+        client_secret=Config.client_secret,
+        access_token=Config.access_token,
+        api_base_url="https://hachyderm.io",
+    )
+
+    token = mastodon.log_in(
+        code=code,
+        redirect_uri="http://localhost:3000/callback",
+        scopes=["read:lists", "read:follows", "read:accounts", "write:lists"],
+    )
+    cookie = uuid.uuid4().urn
+    Database.set(cookie, token)
+
+    cookie_str = f"{cookie}; Domain=localhost; Max-Age={60*60*24}"
+    return {
+        "statusCode": 200,
+        "multiValueHeaders": {"Set-Cookie": f"list-manager-cookie={cookie_str}"},
+        "body": '{"status":"OK"}',
+    }
+
+
 def add_to_list(event, context):
+    """
+    Handler for adding a user to a list.
+
+    Parameters:
+    - list_id - numeric idea of a Mastodon list
+    - account_id - numeric id of a Mastodon user.
+    """
+    cookies = parse_cookies(event["cookies"])
+    cookie = cookies["list-manager-cookie"]
+
     try:
-        mastodon = get_mastodon()
+        mastodon = get_mastodon(cookie)
     except MastodonIllegalArgumentError:
         return {"statusCode": 500, "body": "ERROR"}
     except MastodonInternalServerError:
@@ -115,8 +233,18 @@ def add_to_list(event, context):
 
 
 def remove_from_list(event, context):
+    """
+    Handler for removing a user from a list.
+
+    Parameters:
+    - list_id - numeric idea of a Mastodon list
+    - account_id - numeric id of a Mastodon user.
+    """
+    cookies = parse_cookies(event["cookies"])
+    cookie = cookies["list-manager-cookie"]
+
     try:
-        mastodon = get_mastodon()
+        mastodon = get_mastodon(cookie)
     except MastodonIllegalArgumentError:
         return {"statusCode": 500, "body": "ERROR"}
     except MastodonInternalServerError:
--- list-manager/src/App.js
+++ list-manager/src/App.js
@@ -1,207 +1,29 @@
-import React, { useState, useEffect } from "react";
-import Avatar from "@mui/material/Avatar";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardHeader from "@mui/material/CardHeader";
-import Popover from "@mui/material/Popover";
-import Typography from "@mui/material/Typography";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Manager from "./Manager";
+import Login from "./Login";
+import LoginCallback from "./LoginCallback";
 import "./App.css";
 
-const style = {
-  card: {
-    header: {
-      container: {
-        display: "flex" /* establish flex container */,
-        justifyContent: "space-between",
-        backgroundColor: "lightblue",
-      },
-      padding: 4,
-    },
-    content: {
-      container: {
-        marginBlockStart: "0em",
-      },
-      padding: 4,
-    },
-  },
-};
+/* 
+  /manager is where the manager is stored.
+  / probably has to ask someone to specify a domain and authenticate
+  /auth/{domain} could do this with no UI
+
+  User should be cookied when starting.
+  Backend should store: cookie: {}
+*/
 
 function App() {
-  // Fetcth the data
-  useEffect(() => {
-    fetch("http://localhost:4000/info")
-      .then((resp) => resp.json())
-      .then((data) => {
-        data.followers.sort((a, b) =>
-          a.display_name.localeCompare(b.display_name)
-        );
-        data.lists.sort((a, b) => a.title.localeCompare(b.title));
-        setInfo(data);
-      });
-  }, []);
-
-  // The data
-  const [info, setInfo] = useState({ lists: [], followers: [] });
-  // Popover anchor
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const [follower, setFollower] = React.useState(null);
-
-  const handlePopoverOpen = (event, fol) => {
-    setFollower(fol);
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handlePopoverClose = () => {
-    setAnchorEl(null);
-  };
-
-  // Build the crazy table.
-  const followers = info.followers;
-  const lists = info.lists;
-
-  const listmap = {};
-  lists.forEach((l, index) => {
-    listmap[l.id] = index;
-  });
-
-  const remove = (index, lid) => {
-    const newInfo = { ...info };
-    const fol = newInfo.followers[index];
-    fol.lists = fol.lists.filter((value) => value !== lid);
-    fetch(`http://localhost:4000/remove?list_id=${lid}&account_id=${fol.id}`, {
-      method: "POST",
-    }).then((resp) => {
-      if (resp.ok) {
-        setInfo(newInfo);
-      } else {
-        console.log("An error happened");
-      }
-    });
-  };
-
-  const add = (index, lid) => {
-    const newInfo = { ...info };
-    const fol = newInfo.followers[index];
-    fol.lists.push(lid);
-    fetch(`http://localhost:4000/add?list_id=${lid}&account_id=${fol.id}`, {
-      method: "POST",
-    }).then((resp) => {
-      if (resp.ok) {
-        setInfo(newInfo);
-      } else {
-        console.log("An error happened");
-      }
-    });
-  };
-
-  const open = Boolean(anchorEl);
-
-  // TODO: For really large lists, this will have to be hierarchical at some point.
-
-  const rows = followers.map((fol, index) => {
-    const cols = lists.map((l) => {
-      if (fol.lists.includes(l.id)) {
-        return (
-          <td
-            key={l.id + fol.id}
-            className="cell"
-            onClick={() => remove(index, l.id)}
-          >
-            X
-          </td>
-        );
-      } else {
-        return (
-          <td
-            key={l.id + fol.id}
-            className="cell"
-            onClick={() => add(index, l.id)}
-          >
-            &nbsp;
-          </td>
-        );
-      }
-    });
-    return (
-      <tr>
-        <td align="right" className="usercell">
-          <Typography
-            variant="body2"
-            aria-owns={open ? "mouse-over-popover" : undefined}
-            aria-haspopup="true"
-            onMouseEnter={(evt) => handlePopoverOpen(evt, fol)}
-            onMouseLeave={handlePopoverClose}
-          >
-            <span>{fol.display_name}</span>
-          </Typography>
-        </td>
-        {cols}
-      </tr>
-    );
-  });
-
-  const headers = lists.map((l) => {
-    return (
-      <th key={l.id}>
-        <div key={l.id} className="listname">
-          {l.title}
-        </div>
-      </th>
-    );
-  });
-
-  let popover = <span></span>;
-  if (follower) {
-    popover = (
-      <Popover
-        id="mouse-over-popover"
-        open={open}
-        onClose={handlePopoverClose}
-        anchorEl={anchorEl}
-        anchorOrigin={{
-          vertical: "bottom",
-          horizontal: "right",
-        }}
-        sx={{
-          pointerEvents: "none",
-        }}
-      >
-        <Card sx={{ maxWidth: 400 }}>
-          <CardHeader
-            style={style.card.header}
-            avatar={
-              <Avatar
-                aria-label="user"
-                sx={{ width: 58, height: 58 }}
-                src={follower.avatar}
-              />
-            }
-            title={follower.display_name}
-            subheader={follower.acct}
-          />
-          <CardContent style={style.card.content}>
-            <Typography variant="body2">
-              <span dangerouslySetInnerHTML={{ __html: follower.note }}></span>
-            </Typography>
-          </CardContent>
-        </Card>
-      </Popover>
-    );
-  }
+  //return <Manager />;
 
   return (
-    <div className="App">
-      <table>
-        <thead>
-          <tr>
-            <th>&nbsp;</th>
-            {headers}
-          </tr>
-        </thead>
-        <tbody>{rows}</tbody>
-      </table>
-      {popover}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/manager" element={<Manager />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/callback" element={<LoginCallback />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
