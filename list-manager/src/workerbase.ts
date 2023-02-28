7d649e403fc1e6e8cccfcd5b929d9473828d83c8
Andrew Beers <acbeers@gmail.com>
2023-02-27 17:39:46 -0800

    Add bad hostname to error response (for logging) Add session ID to telemetry and error logging.

--- list-manager/src/workerbase.ts
+++ list-manager/src/workerbase.ts
@@ -1,6 +1,7 @@
 // Baseclass for client and server workers
 
 import { User } from "./types";
+import { v4 as uuidv4 } from "uuid";
 
 // Endpoints
 const urlTelemetry = process.env.REACT_APP_BACKEND_URL + "/telemetry";
@@ -8,11 +9,17 @@ const urlError = process.env.REACT_APP_BACKEND_URL + "/error";
 
 export class WorkerBase {
   protected me: User | null = null;
+  protected session: string = "";
+
+  constructor() {
+    this.session = uuidv4();
+  }
 
   // Logs a telemetry event
   async telemetry(info: Record<string, any>): Promise<void> {
     const data = { ...info };
     if (this.me) data.acct = this.me.acct;
+    data.session = this.session;
 
     return fetch(urlTelemetry, {
       credentials: "include",
@@ -27,6 +34,7 @@ export class WorkerBase {
   async error(info: Record<string, any>): Promise<void> {
     const data = { ...info };
     if (this.me) data.acct = this.me.acct;
+    data.session = this.session;
 
     return fetch(urlError, {
       credentials: "include",
